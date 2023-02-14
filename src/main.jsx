f100063e6c32a77a47bea98f84c1b4cd02d2c928
MatheusGualtieri <50122650+MatheusGualtieri@users.noreply.github.com>
2023-02-14 13:31:48 -0300

    tarefa realizada

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import TodosProvider from "./contexts/todos";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TodosProvider>
+      <App />
+    </TodosProvider>
+  </React.StrictMode>
+);
